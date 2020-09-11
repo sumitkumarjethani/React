c5a879399ede19e26a4b496ad83288881840b783
Sumit Kumar Jethani Jethani <sumitkumar_jethani@hotmail.com>
2020-09-11 01:52:28 +0100

    First Assignment

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle,  } from 'reactstrap';
+
+class DishDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderDish(dish) {
+    return(
+      <Card>
+        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  }
+
+  renderComments(comments) {
+    if (comments != null) {
+      const dishComments = comments.map((comment) => {
+        return (
+          <div key={comment.id}>
+            <li>{comment.comment}</li>
+            <br></br>
+            <li>-- {comment.author}, {comment.date}</li>
+            <br></br>
+          </div>
+        );
+      });
+      return (
+      <div>
+        <ul className="list-unstyled">
+          {dishComments}
+        </ul>
+      </div>);
+    } else {
+      return (<div></div>);
+    }
+  }
+
+  render() {
+    if (this.props.dish != null) {
+      return (
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {this.renderDish(this.props.dish)}
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <h4>Comments</h4>
+            {this.renderComments(this.props.dish.comments)}
+          </div>
+        </div>
+      );
+    } else {
+      return (<div></div>);
+    }
+  }
+}
+export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import DishDetail from './DishdetailComponent';
 
 class Menu extends Component {
   constructor(props) {
@@ -12,22 +12,6 @@ class Menu extends Component {
 
   onDishSelect(dish) {
     this.setState({ selectedDish: dish });
-  }
-
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name}/>
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    } else {
-      return (<div></div>);
-    }
   }
 
   //All Component class have to implement this method (view of the Component)
@@ -51,13 +35,9 @@ class Menu extends Component {
         <div className="row">
           {menu}
         </div>
-        <div className="row">
-          {this.renderDish(this.state.selectedDish)}
-        </div>
+        <DishDetail dish={this.state.selectedDish} />
       </div>
     );
   }
 }
-
-//Export first and then you can import
 export default Menu;
